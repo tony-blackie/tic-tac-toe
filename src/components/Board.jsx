c1f8a2007732d59ad1bef3bc2a03dad875df8bb0
MacBook <jim.casey321@gmail.com>
2017-01-21 17:10:39 +0200

    Check if one of players has won

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -7,11 +7,33 @@ export default class Board extends Component {
 
         this.state = {
             squares: new Array(9).fill(null),
-            xIsNext: true
+            xIsNext: true,
+            winner: null
         };
 
         this.renderSquare = this.renderSquare.bind(this);
         this.makeActive = this.makeActive.bind(this);
+        this.calculateWinner = this.calculateWinner.bind(this);
+    }
+
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
     }
 
     renderSquare(i) {
@@ -21,11 +43,22 @@ export default class Board extends Component {
     makeActive(i) {
         let squares = Object.assign({}, this.state.squares);
         squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({squares: squares, xIsNext: !this.state.xIsNext});
+        this.setState(
+            {
+                squares: squares,
+                xIsNext: !this.state.xIsNext,
+                winner: this.calculateWinner(squares)
+            }
+        );
     }
 
     render() {
-        const status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`;
+        let status;
+        if (!this.state.winner) {
+            status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`;
+        } else {
+            status = `Winner is ${this.state.winner}`;
+        }
         return (
             <div>
                 <div className="status">{status}</div>
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,11 +3,17 @@ import React, { Component } from 'react';
 import Board from './Board.jsx';
 
 export default class Game extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
     render() {
+
         return(
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board calculateWinner={this.calculateWinner} />
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
